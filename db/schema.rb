34a2ffc0f8d758916aaadd9b1b9c5a1f4bf91bf5
natsuki_shimazu <s2natsuki0701s2@gmail.com>
2018-01-20 19:15:39 +0900

    DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405022520) do
+ActiveRecord::Schema.define(version: 20180119013934) do
 
   create_table "captured_images", force: :cascade do |t|
     t.string  "content",      limit: 255
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20160405022520) do
     t.string   "avatar",                 limit: 255
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
+    t.string   "icon",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
