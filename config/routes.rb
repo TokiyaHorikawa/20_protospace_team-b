42a788422467f6456bb00c025ad6238e9fbf37c5
kataokatakuya <kataokatakuya>
2018-01-20 18:59:15 +0900

    Like機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes, only: [:index, :new, :create, :show, :destroy, :edit, :update] do
-    resources :comments, only: [:index, :create]
-   end
+  resources :prototypes, only: [:index, :new, :create, :show, :destroy, :edit] do
+    resources :likes, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
 end
 
