0eef8d613a6c170c1c3324db5d78a90a1d672a9c
kataokatakuya <kataokatakuya>
2018-01-23 16:05:35 +0900

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-
+  get 'prototypes/sort_popular'  => 'prototypes#sort_popular'
   resources :prototypes, only: [:index, :new, :create, :show, :destroy, :edit, :update] do
     resources :comments, only: [:index, :create, :edit, :update, :destroy]
    end
   resources :users, only: [:show, :edit, :update]
+
 end
 
 
