2fcb3a56821c5295c990d1e7a621d2ba7e39fc7e
kataokatakuya <kataokatakuya>
2018-01-23 16:03:58 +0900

    Create js

---
+++ app/assets/javascripts/newest.js
@@ -0,0 +1,55 @@
+$(function() {
+  function appendHTML(prototype){
+    var html = `<div class = "col-sm-4 col-md-3 proto-content">
+                  <div class = "dropdown">
+                  <ul>
+                    <li>
+                      <a data-method = "get" href = "/prototypes/${prototype.id}/edit">編集</a>
+                    </li>
+                    <li>
+                      <a data-confirm="Are you sure?" rel="nofollow" data-method="delete" href="/prorotypes/${prototype.id}">削除</a>
+                    </li>
+                  </ul>
+                  </div>
+                  <div class="thumbnail">
+                    <a href = "/prototypes/${prototype.id}">
+                      <img src="/uploads/captured_image/content/19/mig.jpeg">
+                    </a>
+                    <div class = "caption">
+                      <h3>
+                        ${prototype.title}
+                      </h3>
+                      <div class="proto-meta">
+                        <div class="proto-user">
+                          <a href="/users/${prototype.user_id}">
+                            ${prototype.name}
+                          </a>
+                        </div>
+                        <div class="proto-posted">
+                          ${prototype.posted_date}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>`
+
+    $('.container.proto-list').append(html);
+  }
+
+
+  $("#newest_btn").on('click', function(e){
+    e.preventDefault();
+    $.ajax({
+      type: 'GET',
+      url: '/prototypes',
+      dataType: 'json',
+    })
+    .done(function(data){
+      $('.container.proto-list').empty();
+      data.forEach(function(prototype){
+        appendHTML(prototype);
+      });
+    })
+    return false;
+  })
+});
