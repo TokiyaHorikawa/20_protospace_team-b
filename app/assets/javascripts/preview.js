cd969347d7e3c7bdfb81282f83ca42dee85a0e20
natsuki_shimazu <s2natsuki0701s2@gmail.com>
2018-01-23 14:00:14 +0900

    show _image 最新！！

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,13 +1,11 @@
 $(function(){
 
   //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
-  $('.main_preview').change( function(e) {
-    console.log('hogehoeg')
+  $('input[type=file]').change( function(e) {
     var file = e.target.files[0],
         reader = new FileReader(),
-        $preview = $('.main_preview');
         t = this;
-
+        img = $(this).prev()[0]
 
     // 画像ファイル以外の場合は何もしない
     if(file.type.indexOf("image") < 0){
@@ -17,17 +15,15 @@ $(function(){
     // ファイル読み込みが完了した際のイベント登録
     reader.onload = (function(file) {
       return function(e) {
-        //既存のプレビューを削除
-        $preview.empty();
-        // alert('ok');
+
         // .prevewの領域の中にロードした画像を表示するimageタグを追加
-        $preview.append($('img').attr({
+        $(img).attr({
                   src: e.target.result,
                   width: "100%",
                   height:"100%",
                   class: "preview",
                   title: file.name
-              }));
+        });
       };
     })(file);
 
